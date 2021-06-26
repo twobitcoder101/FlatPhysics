ed1031df642f1bfe4e6c55738da16d4329d60faf
twobitcoder101 <74928485+twobitcoder101@users.noreply.github.com>
2021-06-26 15:31:25 -0600

    Add files via upload

---
+++ FlatVector.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace FlatPhysics
+{
+    public readonly struct FlatVector
+    {
+        public readonly float X;
+        public readonly float Y;
+
+        public static readonly FlatVector Zero = new FlatVector(0f, 0f);
+
+        public FlatVector(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static FlatVector operator +(FlatVector a, FlatVector b)
+        {
+            return new FlatVector(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static FlatVector operator -(FlatVector a, FlatVector b)
+        {
+            return new FlatVector(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static FlatVector operator -(FlatVector v)
+        {
+            return new FlatVector(-v.X, -v.Y);
+        }
+
+        public static FlatVector operator *(FlatVector v, float s)
+        {
+            return new FlatVector(v.X * s, v.Y * s);
+        }
+
+        public static FlatVector operator /(FlatVector v, float s)
+        {
+            return new FlatVector(v.X / s, v.Y / s);
+        }
+
+        public bool Equal(FlatVector other)
+        {
+            return this.X == other.X && this.Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is FlatVector other)
+            {
+                return this.Equal(other);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return new { this.X, this.Y }.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"X: {this.X}, Y: {this.Y}";
+        }
+    }
+}
